61b46ab0336ab15e6adcad57c1a34a7c817135f8
Santiago <santidetaramonaf@gmail.com>
2023-03-03 17:06:12 +0100

    forgot to submit trie class

---
+++ src/Trie.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+struct Trie
+{
+	struct Node
+	{
+		std::unordered_map<char, Node*> children;
+		bool isWordEnd = false;
+	};
+	static void Insert(Node* root, const std::string& key);
+	static bool IsLastNode(struct Node* root);
+	static void SuggestionsRec(Node* root, const std::string& currPrefix, std::vector<std::string>& suggestionsOut);
+	static int GetSuggestions(Node* root, const std::string& query, std::vector<std::string>& suggestionsOut);
+};
