f169b4efcf3491255a728ddb681037a0949c3b28
Santiago <santidetaramonaf@gmail.com>
2023-06-02 00:00:10 +0200

    update generateVsSolution file

--- generateVsSolution.bat
+++ generateVsSolution.bat
@@ -2,11 +2,11 @@
 where premake5
 IF %ERRORLEVEL% NEQ 0 (
 	IF not exist premake-5.0.0-beta1-windows (
-		curl -LO https://github.com/premake/premake-core/releases/download/v5.0.0-beta1/premake-5.0.0-beta1-windows.zip
-		powershell -Command "Expand-Archive premake-5.0.0-beta1-windows.zip"
-		del premake-5.0.0-beta1-windows.zip
+		curl -LO https://github.com/premake/premake-core/releases/download/v5.0.0-beta1/premake-5.0.0-beta2-windows.zip
+		powershell -Command "Expand-Archive premake-5.0.0-beta2-windows.zip"
+		del premake-5.0.0-beta2-windows.zip
 	)
-	premake-5.0.0-beta1-windows\\premake5.exe vs2019
+	premake-5.0.0-beta2-windows\\premake5.exe vs2022
 ) else (
-	premake5 vs2019
+	premake5 vs2022
 )
