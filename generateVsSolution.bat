4f9eca1eff9cb7cc42921ac684d30170e1be5814
Santiago <santidetaramonaf@gmail.com>
2023-06-08 23:18:24 +0200

    fix bat file

--- generateVsSolution.bat
+++ generateVsSolution.bat
@@ -1,8 +1,8 @@
 @echo off
 where premake5
 IF %ERRORLEVEL% NEQ 0 (
-	IF not exist premake-5.0.0-beta1-windows (
-		curl -LO https://github.com/premake/premake-core/releases/download/v5.0.0-beta1/premake-5.0.0-beta2-windows.zip
+	IF not exist premake-5.0.0-beta2-windows (
+		curl -LO https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip
 		powershell -Command "Expand-Archive premake-5.0.0-beta2-windows.zip"
 		del premake-5.0.0-beta2-windows.zip
 	)
