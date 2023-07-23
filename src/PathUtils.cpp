a907bae37dc91b1f868472053a7bc915ef887399
Santiago <san@san.san>
2023-07-23 23:41:30 +0200

    indent with tabs

--- src/PathUtils.cpp
+++ src/PathUtils.cpp
@@ -5,88 +5,88 @@
 
 namespace PathUtils
 {
-    std::string programDirectory;
-    std::string assetsDirectory;
+	std::string programDirectory;
+	std::string assetsDirectory;
 }
 
 void PathUtils::SetProgramDirectory(const std::string& executableFilePath)
 {
-    programDirectory = GetFolderPath(executableFilePath);
-    assetsDirectory = programDirectory + "assets/";
-    if (!std::filesystem::exists(assetsDirectory))
-    {
-        // asset directory is different when launching from visual studio
-        std::string parent = programDirectory;
-        parent = GetFolderPath(parent.substr(0, parent.length() - 1));
-        parent = GetFolderPath(parent.substr(0, parent.length() - 1));
-        parent = GetFolderPath(parent.substr(0, parent.length() - 1));
+	programDirectory = GetFolderPath(executableFilePath);
+	assetsDirectory = programDirectory + "assets/";
+	if (!std::filesystem::exists(assetsDirectory))
+	{
+		// asset directory is different when launching from visual studio
+		std::string parent = programDirectory;
+		parent = GetFolderPath(parent.substr(0, parent.length() - 1));
+		parent = GetFolderPath(parent.substr(0, parent.length() - 1));
+		parent = GetFolderPath(parent.substr(0, parent.length() - 1));
 
-        assetsDirectory = parent + "assets/";
-    }
-    if (!std::filesystem::exists(assetsDirectory))
-        std::cout << "[Path utils] Warning, could not locate assets folder\n";
-    else
-        std::cout << "[Path utils] Assets directory set to: " << assetsDirectory << std::endl;
+		assetsDirectory = parent + "assets/";
+	}
+	if (!std::filesystem::exists(assetsDirectory))
+		std::cout << "[Path utils] Warning, could not locate assets folder\n";
+	else
+		std::cout << "[Path utils] Assets directory set to: " << assetsDirectory << std::endl;
 }
 
 const std::string& PathUtils::GetProgramDirectory()
 {
-    return programDirectory;
+	return programDirectory;
 }
 
 const std::string& PathUtils::GetAssetsDirectory()
 {
-    return assetsDirectory;
+	return assetsDirectory;
 }
 
 std::string PathUtils::GetRelativePath(const std::string& fileRelativeTo, const std::string& targetPath)
 {
-    std::string out = "";
+	std::string out = "";
 
-    std::vector<std::string> pathA, pathB;
+	std::vector<std::string> pathA, pathB;
 
-    int i = fileRelativeTo.length() - 1, iEnd = fileRelativeTo.length();
-    int j = targetPath.length() - 1, jEnd = targetPath.length();
-    while (i > -1)
-    {
-        if (fileRelativeTo[i] == '/' || fileRelativeTo[i] == '\\')
-        {
-            pathA.insert(pathA.begin(), fileRelativeTo.substr(i + 1, iEnd - i - 1));
-            iEnd = i;
-        }
-        i--;
-    }
-    while (j > -1)
-    {
-        if (targetPath[j] == '/' || targetPath[j] == '\\')
-        {
-            pathB.insert(pathB.begin(), targetPath.substr(j + 1, jEnd - j - 1));
-            jEnd = j;
-        }
-        j--;
-    }
+	int i = fileRelativeTo.length() - 1, iEnd = fileRelativeTo.length();
+	int j = targetPath.length() - 1, jEnd = targetPath.length();
+	while (i > -1)
+	{
+		if (fileRelativeTo[i] == '/' || fileRelativeTo[i] == '\\')
+		{
+			pathA.insert(pathA.begin(), fileRelativeTo.substr(i + 1, iEnd - i - 1));
+			iEnd = i;
+		}
+		i--;
+	}
+	while (j > -1)
+	{
+		if (targetPath[j] == '/' || targetPath[j] == '\\')
+		{
+			pathB.insert(pathB.begin(), targetPath.substr(j + 1, jEnd - j - 1));
+			jEnd = j;
+		}
+		j--;
+	}
 
-    i = 0; j = 0;
-    while (pathA[i].compare(pathB[j]) == 0) { i++; j++; }
+	i = 0; j = 0;
+	while (pathA[i].compare(pathB[j]) == 0) { i++; j++; }
 
-    while (i < pathA.size() - 1)
-    {
-        out += "../";
-        i++;
-    }
-    while (j < pathB.size())
-    {
-        out += pathB[j];
-        if (j != pathB.size() - 1)
-            out += '/';
-        j++;
-    }
-    return out;
+	while (i < pathA.size() - 1)
+	{
+		out += "../";
+		i++;
+	}
+	while (j < pathB.size())
+	{
+		out += pathB[j];
+		if (j != pathB.size() - 1)
+			out += '/';
+		j++;
+	}
+	return out;
 }
 
 std::string PathUtils::GetFolderPath(const std::string& filePath)
 {
-    int i = filePath.length() - 1;
-    for (; i > -1 && filePath[i] != '/' && filePath[i] != '\\'; i--) {};
-    return filePath.substr(0, i + 1);
+	int i = filePath.length() - 1;
+	for (; i > -1 && filePath[i] != '/' && filePath[i] != '\\'; i--) {};
+	return filePath.substr(0, i + 1);
 }
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -6,14 +6,14 @@
 
 std::wstring Utils::Utf8ToWstring(const std::string& str)
 {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
-    return myconv.from_bytes(str);
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+	return myconv.from_bytes(str);
 }
 
 void Utils::SetWindowIcon(const std::string& path, GLFWwindow* window)
 {
-    GLFWimage images[1];
-    images[0].pixels = stbi_load(path.c_str(), &images[0].width, &images[0].height, 0, 4); //rgba channels 
-    glfwSetWindowIcon(window, 1, images);
-    stbi_image_free(images[0].pixels);
+	GLFWimage images[1];
+	images[0].pixels = stbi_load(path.c_str(), &images[0].width, &images[0].height, 0, 4); //rgba channels 
+	glfwSetWindowIcon(window, 1, images);
+	stbi_image_free(images[0].pixels);
 }
